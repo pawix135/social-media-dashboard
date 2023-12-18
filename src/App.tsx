442f3dda7b1bbe7c0d1160760fa5126638b9b33c
pawix135 <leagueoflegendsoppl@gmail.com>
2023-12-18 22:31:38 +0100

    overview section styles for desktop and mobile, corrected colors, text sizes in other components

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,10 @@ import Overview from "./components/Overview";
 
 function App() {
   return (
-    <main className="">
-      <div className="absolute left-0 top-0 bg-light-top-bg dark:bg-dark-top-bg w-full h-[300px]"></div>
-      <div className="absolute left-0 top-[300px] bg-light-bg dark:bg-dark-bg w-full h-[calc(100%-300px)]"></div>
-      <div className="container mx-auto relative flex flex-col gap-10 mt-10 px-10 md:px-0">
-        <Header />
-        <Hero />
-        <Overview />
-      </div>
+    <main className="py-10 md:h-screen container mx-auto relative flex flex-col gap-10 px-10 md:px-0">
+      <Header />
+      <Hero />
+      <Overview />
     </main>
   );
 }
--- src/components/OverviewCard/index.tsx
+++ src/components/OverviewCard/index.tsx
@@ -14,13 +14,17 @@ const OverviewCard: React.FC<Props> = ({ overview }) => {
     : overview.value;
 
   return (
-    <div>
-      <div>
-        <span>{overview.name}</span>
+    <div className="bg-light-card-bg dark:bg-dark-card-bg rounded-md p-10 flex flex-col gap-10 hover:bg-light-toggle/50 hover:dark:bg-dark-desaturated-blue/25 hover:cursor-pointer">
+      <div className="flex flex-row items-center justify-between">
+        <span className="dark:text-dark-desaturated-blue font-semibold text-lg">
+          {overview.name}
+        </span>
         <SocialIcon type={overview.type} />
       </div>
-      <div>
-        <span>{displayNumber}</span>
+      <div className="flex flex-row items-center justify-between">
+        <span className="text-3xl font-bold text-light-dark-blue dark:text-dark-text">
+          {displayNumber}
+        </span>
         <SocialNuberChange change={overview.change} type="percentage" />
       </div>
     </div>
