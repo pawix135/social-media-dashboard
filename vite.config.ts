c28d5ef10d67994754ec6ab76216bd5bd42525ef
pawix135 <leagueoflegendsoppl@gmail.com>
2023-12-18 18:38:40 +0100

    App initialized with tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/social-media-dashboard/",
+  plugins: [react()],
+});
